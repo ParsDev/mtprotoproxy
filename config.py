baf0c1eacc3e13d0ef82af4ca6cbfd6f6dfebeb1
ParsDev <de3ktop@gmail.com>
2019-08-26 18:13:37 +0430

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ PORT = 3256
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000000",
-    "tg2": "0123456789abcdef0123456789abcdef"
+    "tg":  "00000000000000000000000000000001",
 }
 
 # Makes the proxy harder to detect
@@ -12,11 +11,11 @@ SECURE_ONLY = True
 
 # Makes the proxy even more hard to detect
 # Compatible only with the recent clients
-# TLS_ONLY = True
+TLS_ONLY = True
 
 # The domain for TLS, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "www.google.com"
 
 # Tag for advertising, obtainable from @MTProxybot
-# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+AD_TAG = "2b90a147687a0986d4af9f8d2907d676"
